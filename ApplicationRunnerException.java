ba39ac021bfa138a15209a09b34ba68a451caba6
Zack099 <82106504+Zack099@users.noreply.github.com>
2021-04-12 20:29:02 +0200

    Exception debug

---
+++ ApplicationRunnerException.java
@@ -0,0 +1,13 @@
+package org.springframework.boot;
+
+public class ApplicationRunnerException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ApplicationRunnerException(String msg) {
+		super(msg);
+	}
+
+}
